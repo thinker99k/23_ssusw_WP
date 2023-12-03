226a9af1029d1e6dbae458549d3c771fb9209d84
Jeff Lee <114413811+kitsune03k@users.noreply.github.com>
2023-12-03 13:21:25 +0900

    ㅇㅇ

---
+++ Modal_Loading.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// Modal_Loading 대화 상자
+
+class Modal_Loading : public CDialogEx
+{
+	DECLARE_DYNAMIC(Modal_Loading)
+
+public:
+	Modal_Loading(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~Modal_Loading();
+
+	// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG_LOADING };
+#endif
+private:
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+	BOOL OnInitDialog();
+	UINT_PTR m_nTimer; // 타이머 ID
+	DECLARE_MESSAGE_MAP();
+
+public:
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+};
